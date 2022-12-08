17d9838a291bb9e539ab9dca722c247157bd0f77
jinh013 <jmrandrianant@gmail.com>
2022-12-08 18:11:01 +0300

    npm i react react-dom npm install --save-dev @babel/core babel-loader @babel/preset-env @babel/preset-react create a public folder in the root of the project, and create a file index.html - npm install --save-dev html-webpack-plugin - npm install --save-dev webpack-dev-server - "scripts": {     "test": "echo \"Error: no test specified\" && exit 1",     "start": "webpack serve --mode=development",     "build": "webpack --mode production"   }, npm install --save-dev html-webpack-plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log("monia");
+import ReactDOM from "react-dom/client";
+function App() {
+  return <h1>Rick and Morty</h1>;
+}
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(<App />);
